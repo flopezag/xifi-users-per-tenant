ab749fb95b5e492874d81bbb0a432308e5e71386
Fernando Lopez Aguilar <fla@tid.es>
2014-06-18 14:05:53 +0200

    Minus changes...

--- users-per-tenant.py
+++ users-per-tenant.py
@@ -1,17 +1,17 @@
-mport sys, MySQLdb
+import sys, MySQLdb, json
+
+host = 'localhost'
+user = 'root'
+password = 'root'
 
 def PrintTenantId(database, table, tenant):
     """ Connects to the table specified by the user and prints out its fields in HTML format used by Ben's wiki. """
-    host = 'localhost'
-    user = 'root'
-    password = 'root'
     conn = MySQLdb.Connection(db=database, host=host, user=user, passwd=password)
     mysql = conn.cursor()
     sql = """ SELECT id FROM %s WHERE name=\"%s\" """ % (table, tenant)
     print sql
     mysql.execute(sql)
     fields=mysql.fetchall()
-    counter = 0
     for field in fields:
         id = field[0]
     
@@ -22,13 +22,23 @@ def PrintTenantId(database, table, tenant):
 
 
 def PrintEmailUsersPerTenant(database, table, tenantId):
-    print "fin"
+    conn = MySQLdb.Connection(db=database, host=host, user=user, passwd=password)
+    mysql = conn.cursor()
+    sql = """ SELECT extra FROM %s """" % (table)
+    print sql
+    mysql.execute(sql)
+    fields=mysql.fetchall()
+    for field in fields:
+        print field
+        data = json.load(field)
+        print data['tenantId']
+
 
 
 users_database = sys.argv[1]
 users_table = sys.argv[2]
 users_tenant = sys.argv[3]
-print "Wikified HTML for " + users_database + "." + users_table
+print "Wikified txt for " + users_database + "." + users_table
 print "========================"
 
 tenantId = PrintTenantId(users_database, users_table, users_tenant)
